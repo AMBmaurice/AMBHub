f4ada8576f3f8ac8b8fc0b612e961a7e45ea2692
AMBmaurice <admin@amb-maurice.fr>
2026-07-22 07:23:41 +0200

    Implement remember me feature in login
    Added remember me functionality to login process.

--- core/views.py
+++ core/views.py
@@ -10,20 +10,28 @@ def page_connexion(request):
     if request.method == 'POST':
         identifiant = request.POST.get('username')
         mot_de_passe = request.POST.get('password')
+        se_souvenir = request.POST.get('remember_me')
         
         # Vérification des identifiants dans la base de données
         user = authenticate(request, username=identifiant, password=mot_de_passe)
         
         if user is not None:
             login(request, user)
-            return redirect('hub') # Redirection vers le Hub si tout est bon
+            
+            if se_souvenir:
+                # Conserve la session pendant 10 jours (864 000 secondes)
+                request.session.set_expiry(864000)
+            else:
+                # La session expire dès la fermeture du navigateur
+                request.session.set_expiry(0)
+                
+            return redirect('hub')
         else:
             messages.error(request, "Identifiant ou mot de passe incorrect.")
             
     return render(request, 'login.html')
 
 def page_hub(request):
-    # Sécurité : Si un intrus essaie d'aller sur /hub sans être connecté, on le renvoie à l'accueil
     if not request.user.is_authenticated:
         return redirect('connexion')
     return render(request, 'hub.html')
